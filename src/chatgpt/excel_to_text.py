c2fca0b812ca6299bf5cee3d3bf4a777bfa3dca2
Gonçalo Machado <goncalofmachado01@gmail.com>
2024-04-22 20:09:34 +0100

    add dataset and code to convert excel to txt

---
+++ src/chatgpt/excel_to_text.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+  
+df = pd.DataFrame(pd.read_excel(".\example\ieee-init.xlsx")) 
+  
+df = df.drop('id', axis=1)
+df = df.drop('keyword', axis=1)
+df = df.drop('title', axis=1)
+
+with open('.\example.\human_text.txt', 'w', errors='ignore') as file:
+
+    for index, row in df.iterrows():
+        text = row['abstract']
+        file.write(text)
+
+df = pd.DataFrame(pd.read_excel(".\example\ieee-chatgpt-generation.xlsx")) 
+  
+df = df.drop('id', axis=1)
+df = df.drop('keyword', axis=1)
+df = df.drop('title', axis=1)
+
+with open('.\example.\chatgpt_text.txt', 'w', errors='ignore') as file:
+
+    for index, row in df.iterrows():
+        text = row['abstract']
+        file.write(text)
