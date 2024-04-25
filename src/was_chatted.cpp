e6306e1644655ad00bd12f51a57d04156fd8373d
Gonçalo Machado <goncalofmachado01@gmail.com>
2024-04-25 16:38:08 +0100

    was_chatted works. Need to do tests to see accuracy.

--- src/was_chatted.cpp
+++ src/was_chatted.cpp
@@ -4,12 +4,19 @@
 #include <sstream>
 #include <fstream>
 #include <unordered_map>
+#include <math.h>
 
 using namespace std;
 
-unordered_map<string, unordered_map<char, int>> markv_freq;
+unordered_map<string, unordered_map<char, int>> humanModel;
+unordered_map<string, unordered_map<char, int>> chatGPTModel;
 
-string inputFilename = "none";   // File name
+string humanTextFilename = "none";
+string chatGPTTextFilename = "none";
+string targetTextFilename = "none";
+
+int alpha = 1;
+int k = 3;
 
 unordered_map<string, string> inputflags;
 
@@ -39,38 +46,39 @@ unordered_map<string, string> getFlags(int argc, char* argv[]) {
     return flags;
 }
 
-int main(int argc, char* argv[])
-{
+unordered_map<string, unordered_map<char, int>> getModel(string filename, string modelType, int k) {
 
-    // get input paramenters
-    inputflags = getFlags(argc, argv);
-    if (inputflags.count("f")) {
-        inputFilename = inputflags.at("f");
-    }
-    else {
-        std::cout << "No filename given\n";
-        return 1;
-    }
+    unordered_map<string, unordered_map<char, int>> map;
 
-    std::cout << "-> File: " << inputFilename << endl;
+    map.clear();
 
-    ifstream file(inputFilename);
+    ifstream file(filename);
 
     if (!file.is_open())
     {
-        std::cout << "FCM: Error opening file: " << inputFilename;
-        return 1;
+        std::cout << "Error opening file: " << filename << endl;
+        return map;
     }
 
     string line = "";
 
-    string modelType;
-    int k;
+    string fileModelType;
+    int fileK;
 
-    getline(file, modelType);
+    getline(file, fileModelType);
 
     getline(file, line);
-    k = stoi(line);
+    fileK = stoi(line);
+
+    if (modelType != fileModelType) {
+        std::cout << "This file is does not have the expected model type " << modelType << ". File model type :  " << fileModelType << endl;
+        return map;
+    }
+
+    if (k != fileK) {
+        std::cout << "This file is not of the expected k " << to_string(k) << ". File k : " << to_string(fileK) << endl;
+        return map;
+    }
 
     size_t mapSize;
     getline(file, line);
@@ -78,20 +86,16 @@ int main(int argc, char* argv[])
     sstream >> mapSize;
 
     for (size_t i = 0; i < mapSize; ++i) {
-        // Read the outer key
         string outerKey;
         getline(file, outerKey);
 
-        // Read the size of the inner map
         size_t innerMapSize;
         getline(file, line);
         std::stringstream sstream(line);
         sstream >> innerMapSize;
 
-        // Create the inner map
         unordered_map<char, int> innerMap;
 
-        // Read key-value pairs of the inner map
         for (size_t j = 0; j < innerMapSize; ++j) {
             string innerKey;
             getline(file, innerKey);
@@ -108,22 +112,15 @@ int main(int argc, char* argv[])
             innerMap[innerKey[0]] = innerValue;
         }
 
-        // Insert the inner map into the outer map
-        markv_freq[outerKey] = innerMap;
+        map[outerKey] = innerMap;
 
     }
 
-    // Close the file
     file.close();
-
-    // Print the values of the two variables
-    std::cout << "Model Type: " << modelType << endl;
-    std::cout << "k: " << k << endl;
-    std::cout << "Size of map: " << markv_freq.size() << endl;
 
     //// Print the contents of the map
     //std::cout << "Map contents:" <<endl;
-    //for (const auto& pair : markv_freq) {
+    //for (const auto& pair : map) {
     //    std::cout << "Outer Key: " << pair.first << endl;
     //    std::cout << "Inner Map:" << endl;
     //    for (const auto& innerPair : pair.second) {
@@ -131,6 +128,138 @@ int main(int argc, char* argv[])
     //    }
     //    std::cout << endl;
     //}
+
+    return map;
+}
+
+int getTotalCounts(unordered_map<char, int> map) {
+    int counter = 0;
+    for (const auto& pair : map) {
+        counter += pair.second;
+    }
+    return counter;
+}
+
+int main(int argc, char* argv[])
+{
+    inputflags = getFlags(argc, argv);
+    if (inputflags.count("h")) {
+        humanTextFilename = inputflags.at("h");
+    }
+    else {
+        std::cout << "No human text filename given" << endl;
+        return 1;
+    }
+
+    if (inputflags.count("c")) {
+        chatGPTTextFilename = inputflags.at("c");
+    }
+    else {
+        std::cout << "No human text filename given" << endl;
+        return 1;
+    }
+
+    if (inputflags.count("t")) {
+        targetTextFilename = inputflags.at("t");
+    }
+    else {
+        std::cout << "No human text filename given" << endl;
+        return 1;
+    }
+
+    if (inputflags.count("a")) {
+        alpha = stoi(inputflags.at("a"));
+    }
+    else {
+        std::cout << "No alpha given" << endl;
+        return 1;
+    }
+
+    if (inputflags.count("k")) {
+        k = stoi(inputflags.at("k"));
+    }
+    else {
+        std::cout << "No k given" << endl;
+        return 1;
+    }
+
+    std::cout << "-> Human Text Filename: " << humanTextFilename << endl;
+    std::cout << "-> ChatGPT Text Filename: " << chatGPTTextFilename << endl;
+    std::cout << "-> Target Text Filename: " << targetTextFilename << endl;
+    std::cout << "-> Alpha: " << to_string(alpha) << endl;
+    std::cout << "-> K: " << to_string(k) << endl;
+
+    chatGPTModel = getModel(chatGPTTextFilename, "A", k);
+    humanModel = getModel(humanTextFilename, "H", k);
+
+    ifstream file(targetTextFilename);
+
+    if (!file.is_open())
+    {
+        std::cout << "Error opening file: " << targetTextFilename << endl;
+        return 1;
+    }
+
+    double humanModelInfo = 0;
+    double chatGPTModelInfo = 0;
+
+    int humanModelSymbolCount = 0;
+    int chatGPTModelSymbolCount = 0;
+
+    int humanModelTotalSymbolCount = 0;
+    int chatGPTModelTotalSymbolCount = 0;
+
+    double probability = 0;
+    double bits = 0;
+    char letter;
+    string window = "";
+
+    while (file.get(letter)) {
+        if (letter == '\n' || letter == '\t') {
+            continue;
+        }
+
+        if (window.size() == k) {
+
+            chatGPTModelSymbolCount = chatGPTModel[window][letter];
+            chatGPTModelTotalSymbolCount = getTotalCounts(chatGPTModel[window]);
+
+            probability = (double)(chatGPTModelSymbolCount + alpha) / (double)(chatGPTModelTotalSymbolCount + alpha*k);
+
+            chatGPTModelInfo += -log2(probability);
+
+            humanModelSymbolCount = humanModel[window][letter];
+            humanModelTotalSymbolCount = getTotalCounts(humanModel[window]);
+
+            probability = (double)(humanModelSymbolCount + alpha) / (double)(humanModelTotalSymbolCount + alpha * k);
+
+            humanModelInfo += -log2(probability);
+
+        }
+
+        if (window.length() == k) {
+            window.erase(0, 1);
+        }
+
+        window += letter;
+    }
+
+    std::cout << "Human Model Info: " << to_string(humanModelInfo) << endl;
+    std::cout << "ChatGPT Model Info: " << to_string(chatGPTModelInfo) << endl;
+
+    string targetType = "";
+
+    if (humanModelInfo < chatGPTModelInfo) {
+        targetType = "Human";
+    }
+    else if (humanModelInfo > chatGPTModelInfo) {
+        targetType = "ChatGPT";
+    }
+    else {
+        targetType = "Unknown";
+    }
+
+    std::cout << "Target Text Type : " << targetType << endl;
 
     return 0;
 }
